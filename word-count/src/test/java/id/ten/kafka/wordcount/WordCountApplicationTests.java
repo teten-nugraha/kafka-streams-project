7abbf4957f54440cde56c3f4f37d39ea9f328d08
Teten Nugraha <teten.nugraha@allianz.co.id>
2020-12-23 16:22:19 +0700

    word count

---
+++ word-count/src/test/java/id/ten/kafka/wordcount/WordCountApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ten.kafka.wordcount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordCountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
