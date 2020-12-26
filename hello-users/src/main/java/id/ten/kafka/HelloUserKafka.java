8345a2642bb0d9d180d3b58c8e7b78d54a7f8833
teten <tennugraha777@gmail.com>
2020-12-26 11:41:52 +0700

    hello users

---
+++ hello-users/src/main/java/id/ten/kafka/HelloUserKafka.java
@@ -0,0 +1,37 @@
+package id.ten.kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.KStream;
+
+import java.util.Properties;
+
+public class HelloUserKafka {
+    public static void main(String[] args) {
+
+        Properties config = new Properties();
+        config.put(StreamsConfig.APPLICATION_ID_CONFIG, "hello-users-application");
+        config.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        config.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        config.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+
+        StreamsBuilder builder = new StreamsBuilder() ;
+
+        KStream<String, String> stream = builder.stream("users");
+
+        stream.foreach((key, value) -> {
+            System.out.println("(DSL) Hello, "+ value);
+        });
+
+        KafkaStreams streams = new KafkaStreams(builder.build(), config);
+        streams.start();
+
+        // close Kafka Streams when the JVM shuts down (e.g. SIGTERM)
+        Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
+
+    }
+}
